a46f8cf9aee3796fe09b203fcd14ede36d9ed1c8
Chris Auret <chris.auret@yahoo.co.uk>
2017-01-26 13:05:02 +1100

    first commit

---
+++ index.android.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+  <Text>Jenna has ass problems</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
