f46d9e96183e8c50d3bd959aa49f8a21f4c56abb
Chris Auret <chris.auret@yahoo.co.uk>
2017-01-29 22:24:16 +1100

    header styles

--- src/components/header.js
+++ src/components/header.js
@@ -16,11 +16,16 @@ const styles = {
     fontSize: 20
   },
   viewStyle: {
-    backgroundColor: '#14F778',
+    backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
     height: 60,
-    paddingTop: 15
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    elevation: 2,
+    position: 'relative'
   }
 };
 
