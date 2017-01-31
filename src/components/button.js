ada48bc6b35c801e1fd02153a2d1e6a91a1dc487
Chris Auret <chris.auret@yahoo.co.uk>
2017-01-31 13:20:04 +1100

    improve button style

--- src/components/button.js
+++ src/components/button.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
+
 const Button = () => {
+  const { buttonStyle, textStyle } = styles;
+
   return (
-    <TouchableOpacity>
-      <Text>Click me!!</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>
+        Click me!!
+      </Text>
     </TouchableOpacity>
   );
+};
+
+const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch', // fill limits of container
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007AFF',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007AFF',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
 };
 
 export default Button;
